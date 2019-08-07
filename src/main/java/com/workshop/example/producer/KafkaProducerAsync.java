9a0518406ab276e327099ac3c1f825a6da92fe08
Simon Streubel <simon.streubel@rheindata.com>
2019-08-07 12:10:44 +0200

    initial commit

---
+++ src/main/java/com/workshop/example/producer/KafkaProducerAsync.java
@@ -0,0 +1,64 @@
+package com.workshop.example.producer;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.LocalDateTime;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class KafkaProducerAsync {
+
+    private final static String TOPIC = "test-java-topic";
+    private final static String BOOTSTRAP_SERVERS
+            = "localhost:29092,localhost:29093";
+
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+
+    private static void runProducer(int sendMessageCount, int waitMsInBetween, long id) throws Exception {
+        Long key = (id > 0) ? id : null;
+        final CountDownLatch countDownLatch = new CountDownLatch(sendMessageCount);
+
+        try (Producer<Long, String> producer = createProducer()) {
+            for (int index = 0; index < sendMessageCount; index++) {
+                long time = System.currentTimeMillis();
+
+                ProducerRecord<Long, String> record
+                        = new ProducerRecord<>(TOPIC, key,
+                        "[" + id + "] Hello Kafka " + LocalDateTime.now());
+
+                producer.send(record, (metadata, exception) -> {
+                    long elapsedTime = System.currentTimeMillis() - time;
+                    if (metadata != null) {
+                        System.out.printf("[" + id + "] sent record(key=%s value=%s) "
+                                        + "meta(partition=%d, offset=%d) time=%d\n",
+                                record.key(), record.value(), metadata.partition(),
+                                metadata.offset(), elapsedTime);
+                    } else {
+                        exception.printStackTrace();
+                    }
+                    countDownLatch.countDown();
+                });
+            }
+        }
+    }
+
+
+    public static void main(String... args) throws Exception {
+        if (args.length == 0) {
+            runProducer(100, 10, 99);
+        } else {
+            runProducer(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Long.parseLong(args[2]));
+        }
+    }
+}
+
+
